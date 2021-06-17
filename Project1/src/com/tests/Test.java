fa9c5b8a5ac726a139c332cdf0646d7ccb1615b1
dev2@gmail.com <hemanth@hemanth-PC>
2021-06-17 11:37:19 +0530

    Dev1 modified Test.java file

--- Project1/src/com/tests/Test.java
+++ Project1/src/com/tests/Test.java
@@ -1,5 +1,12 @@
 package com.tests;
 
-public class Test {
+import com.beans.Utility;
 
+public class Test
+{
+public static void main(String args[])
+{
+Utility utility=new Utility();
+System.out.println(utility.getSystemDate());
+}
 }
